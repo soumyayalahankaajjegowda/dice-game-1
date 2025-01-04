75bcb495f225a3094f3557b5ea56d378a1b511b5
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-01-03 22:29:19 -0700

    lastprogress

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-
 let player1 = "player1";
 let player2 = "player2";
 
+function editName() {
+    player1 = prompt("Change player1 Name");
+    player2 = prompt("Change player2 Name");
 
-
-
+    if (player1.length < 1 || player2.length < 1) {
+        alert('Please enter a valid name');
+        return;
+    }
+    document.querySelector("p.player1").innerHTML = player1;
+    document.querySelector("p.player2").innerHTML = player2;
+}
 
 function rollTheDice() {
-   let diceNum1 = document.querySelector(".img1");
-   let diceNum2 = document.querySelector(".img2");
+    let diceNum1 = document.querySelector(".img1");
+    let diceNum2 = document.querySelector(".img2");
 
-   diceNum1.setAttribute("src", "diceroll.gif")
-   diceNum2.setAttribute("src", "diceroll.gif")
+    diceNum1.setAttribute("src", "diceroll.gif");
+    diceNum2.setAttribute("src", "diceroll.gif");
 
-   let result = document.querySelector('h1')
-   result.innerHTML = ""
-   setTimeout(() => {
-     let randomNumber1 = Math.floor(Math.random()*6) + 1;
-     let randomNumber2 = Math.floor(Math.random()*6) + 1;
+    let result = document.querySelector('h1');
+    result.innerHTML = "";
+    setTimeout(() => {
+        let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-     diceNum1.setAttribute('src', 'dice' + randomNumber1 + '.png');
-     diceNum2.setAttribute('src', 'dice' + randomNumber2 + '.png');
+        diceNum1.setAttribute('src', 'dice' + randomNumber1 + '.png');
+        diceNum2.setAttribute('src', 'dice' + randomNumber2 + '.png');
 
-     //determine the winner
-     if (randomNumber1 === randomNumber2) {
-        result.innerHTML = "Draw!"
-     }
-     else if (randomNumber1 < randomNumber2) {
-        result.innerHTML = (player2 + "WINS!");
-     }
-     else {
-        result.innerHTML = (Player1 + "WINS!");
-     }
-   }, 2500);
+        // Determine the winner
+        if (randomNumber1 === randomNumber2) {
+            result.innerHTML = "Draw!";
+        } else if (randomNumber1 < randomNumber2) {
+            result.innerHTML = (player2 + " WINS!");
+        } else {
+            result.innerHTML = (player1 + " WINS!");
+        }
+    }, 2500);
 }
